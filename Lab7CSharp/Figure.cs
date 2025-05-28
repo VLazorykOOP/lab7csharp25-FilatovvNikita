607db66fd4a9a2f5bf9cfa3f5c010f5beeb0a03f
FilatovvNikita <filatov.nikita@chnu.edu.ua>
2025-05-28 18:59:51 +0300

    lab7

---
+++ Lab7CSharp/Figure.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LR7
+{
+    abstract class Figure
+    {
+        public int X, Y;
+        public Color Color;
+        public string Text;
+        public abstract void Draw(Graphics g);
+        public abstract void Move(int dx, int dy);
+    }
+
+    class Square : Figure
+    {
+        public int Size;
+
+        public Square(int x, int y, Color color, string text, int size)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            Text = text;
+            Size = size;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color, 2);
+            g.DrawRectangle(pen, X, Y, Size, Size);
+            g.DrawString(Text, SystemFonts.DefaultFont, Brushes.Black, X + Size / 4, Y + Size / 4);
+        }
+
+        public override void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+
+    class Rectangle : Figure
+    {
+        public int Width, Height;
+
+        public Rectangle(int x, int y, Color color, string text, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            Text = text;
+            Width = width;
+            Height = height;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color, 2);
+            g.DrawRectangle(pen, X, Y, Width, Height);
+            g.DrawString(Text, SystemFonts.DefaultFont, Brushes.Black, X + Width / 4, Y + Height / 4);
+        }
+
+        public override void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+
+    class Ellipse : Figure
+    {
+        public int Width;
+        public int Height;
+
+        public Ellipse(int x, int y, Color color, string text, int width, int height)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            Text = text;
+            Width = width;
+            Height = height;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color, 2);
+            g.DrawEllipse(pen, X - Width / 2, Y - Height / 2, Width, Height);
+            g.DrawString(Text, SystemFonts.DefaultFont, Brushes.Black, X - Width / 4, Y - Height / 4);
+        }
+
+        public override void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+
+
+    class Rhombus : Figure
+    {
+        public int Size;
+
+        public Rhombus(int x, int y, Color color, string text, int size)
+        {
+            X = x;
+            Y = y;
+            Color = color;
+            Text = text;
+            Size = size;
+        }
+
+        public override void Draw(Graphics g)
+        {
+            Pen pen = new Pen(Color, 2);
+            Point[] points = {
+            new Point(X, Y - Size / 2),
+            new Point(X + Size / 2, Y), 
+            new Point(X, Y + Size / 2),
+            new Point(X - Size / 2, Y) 
+        };
+            g.DrawPolygon(pen, points);
+            g.DrawString(Text, SystemFonts.DefaultFont, Brushes.Black, X - Size / 4, Y - Size / 4);
+        }
+
+        public override void Move(int dx, int dy)
+        {
+            X += dx;
+            Y += dy;
+        }
+    }
+
+}
--- Lab7CSharp/Form1.cs
+++ Lab7CSharp/Form1.cs
@@ -1,25 +1,186 @@
-п»їusing System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Lab7CSharp
+namespace LR7
 {
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
         }
 
-        private void label1_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
+            if (!string.IsNullOrWhiteSpace(textBox1.Text))
+            {
+                comboBox1.Items.Add(textBox1.Text);
+                textBox1.Clear();
+            }
+            else
+            {
+                MessageBox.Show("Введіть текст елементу.");
+            }
+        }
 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Items.Count > 0)
+            {
+                comboBox1.Items.RemoveAt(comboBox1.Items.Count - 1);
+            }
+            else
+            {
+                MessageBox.Show("Немає елементів для видалення.");
+            }
+        }
+
+
+        // Завдання друге
+
+        private Bitmap originalImage;
+        private Bitmap grayscaleImage;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openDlg = new OpenFileDialog();
+            openDlg.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp";
+
+            if (openDlg.ShowDialog() == DialogResult.OK)
+            {
+                originalImage = new Bitmap(openDlg.FileName);
+                pictureBox1.Image = originalImage;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (grayscaleImage == null)
+            {
+                MessageBox.Show("Спочатку потрібно перетворити зображення.");
+                return;
+            }
+
+            SaveFileDialog saveDlg = new SaveFileDialog();
+            saveDlg.Filter = "PNG Image|*.png|JPEG Image|*.jpg|Bitmap Image|*.bmp";
+
+            if (saveDlg.ShowDialog() == DialogResult.OK)
+            {
+                grayscaleImage.Save(saveDlg.FileName);
+                MessageBox.Show("Зображення збережено.");
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (originalImage == null)
+            {
+                MessageBox.Show("Будь ласка, відкрийте зображення спочатку.");
+                return;
+            }
+
+            grayscaleImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int y = 0; y < originalImage.Height; y++)
+            {
+                for (int x = 0; x < originalImage.Width; x++)
+                {
+                    Color originalColor = originalImage.GetPixel(x, y);
+
+                    int grayValue = (int)((originalColor.R + originalColor.G + originalColor.B) / 3);
+                    Color grayColor = Color.FromArgb(grayValue, grayValue, grayValue);
+
+                    grayscaleImage.SetPixel(x, y, grayColor);
+                }
+            }
+
+            pictureBox1.Image = grayscaleImage;
+        }
+
+        // Завдання третє
+        private Figure[] figures;
+        private Random rand = new Random();
+        private Bitmap bmp;
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox2.Text);
+            figures = new Figure[n];
+
+            bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            Graphics g = Graphics.FromImage(bmp);
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = rand.Next(20, pictureBox2.Width - 40);
+                int y = rand.Next(20, pictureBox2.Height - 40);
+                Color color = Color.FromName(comboBox3.SelectedItem.ToString());
+                string text = textBox3.Text;
+
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "Square":
+                        figures[i] = new Square(x, y, color, text, int.Parse(textBox4.Text));
+                        break;
+                    case "Rectangle":
+                        figures[i] = new Rectangle(x, y, color, text, int.Parse(textBox5.Text), int.Parse(textBox6.Text));
+                        break;
+                    case "Ellipse":
+                        figures[i] = new Ellipse(x, y, color, text, int.Parse(textBox7.Text), int.Parse(textBox8.Text));
+            break;
+                    case "Rhombus":
+                        figures[i] = new Rhombus(x, y, color, text, int.Parse(textBox9.Text));
+                        break;
+                }
+                figures[i].Draw(g);
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox2.Text);
+            figures = new Figure[n];
+
+            if (bmp == null)
+                bmp = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+
+            Graphics g = Graphics.FromImage(bmp);
+
+            for (int i = 0; i < n; i++)
+            {
+                int x = rand.Next(20, pictureBox2.Width - 40);
+                int y = rand.Next(20, pictureBox2.Height - 40);
+                Color color = Color.FromName(comboBox3.SelectedItem.ToString());
+                string text = textBox3.Text;
+
+                switch (comboBox2.SelectedItem.ToString())
+                {
+                    case "Square":
+                        figures[i] = new Square(x, y, color, text, int.Parse(textBox4.Text));
+                        break;
+                    case "Rectangle":
+                        figures[i] = new Rectangle(x, y, color, text, int.Parse(textBox5.Text), int.Parse(textBox6.Text));
+                        break;
+                    case "Ellipse":
+                        figures[i] = new Ellipse(x, y, color, text, int.Parse(textBox7.Text), int.Parse(textBox8.Text));
+                        break;
+                    case "Rhombus":
+                        figures[i] = new Rhombus(x, y, color, text, int.Parse(textBox9.Text));
+                        break;
+                }
+                figures[i].Draw(g);
+            }
+            pictureBox2.Image = bmp;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pictureBox2.Image = null;
+            bmp = null;
+
+            comboBox2.SelectedIndex = 0;
+            comboBox3.SelectedIndex = 0;
         }
     }
 }
